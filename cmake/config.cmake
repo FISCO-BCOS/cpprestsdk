7cbaab1d81ba4bbdca8b7fb0d872c1155d6bbfc5
cyjseagull <yujiechen_hust@163.com>
2022-04-25 22:19:34 +0800

    cpprestsdk support hunter

---
+++ cmake/config.cmake
@@ -0,0 +1,16 @@
+hunter_config(
+    Boost VERSION "1.76.0-local"
+    URL
+    "https://osp-1257653870.cos.ap-guangzhou.myqcloud.com/FISCO-BCOS/FISCO-BCOS/deps/boost_1_76_0.tar.bz2
+    https://downloads.sourceforge.net/project/boost/boost/1.76.0/source/boost_1_76_0.tar.bz2
+    https://nchc.dl.sourceforge.net/project/boost/boost/1.76.0/boost_1_76_0.tar.bz2"
+    SHA1
+    8064156508312dde1d834fec3dca9b11006555b6
+    CMAKE_ARGS
+    CONFIG_MACRO=BOOST_UUID_RANDOM_PROVIDER_FORCE_POSIX
+)
+
+hunter_config(OpenSSL VERSION tassl_1.1.1b_v1.4-local
+    URL https://${URL_BASE}/FISCO-BCOS/TASSL-1.1.1b/archive/f9d60fa510e5fbe24413b4abdf1ea3a48f9ee6aa.tar.gz
+    SHA1 e56121278bf07587d58d154b4615f96575957d6f
+)
